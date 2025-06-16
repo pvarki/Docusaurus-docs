1f39fae4389fb8d8d357ec00ca51eabe7a9fcc3e
Lonimokio <Arttu.mutka@gmail.com>
2025-06-16 13:14:00 +0300

    feat: restructure sidebar categories and update client cache directory

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/home/lonimokio/Programming/Docusaurus-docs/tina/__generated__/.cache/1750056003361', url: 'https://content.tinajs.io/1.5/content/aa3b3372-d1e3-4953-93aa-2ff36c3302da/github/main', token: '2812ff341972e65bbcf3ba455337c17b608af184', queries,  });
+export const client = createClient({ cacheDir: '/home/lonimokio/Programming/Docusaurus-docs/tina/__generated__/.cache/1750067627452', url: 'https://content.tinajs.io/1.5/content/aa3b3372-d1e3-4953-93aa-2ff36c3302da/github/main', token: '2812ff341972e65bbcf3ba455337c17b608af184', queries,  });
 export default client;
   
